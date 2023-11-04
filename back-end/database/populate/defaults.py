7278bcd17f42906ce66b108e3e6b1ce571dbe7d6
chloetkl <e0889098@u.nus.edu>
2023-11-05 00:20:00 +0800

    Connected successfully to SQL server in defaults.py. Adding in defaults

---
+++ back-end/database/populate/defaults.py
@@ -0,0 +1,51 @@
+import mysql.connector
+import pandas as pd
+
+def establish_sql_connection():
+    # note: for testing purposes, run on localhost
+    db = mysql.connector.connect(
+        host = "localhost",
+        port = 1001,
+        user = "root",
+        password = "dsa3101",
+        database = "secdb"
+        )
+    # uncomment below for dockerised usage
+    # db = mysql.connector.connect(
+    #     host = "database",
+    #     user = "root",
+    #     password = "dsa3101",
+    #     database = "secdb"
+    #     )
+    return db
+
+def add_user_roles(filepath):
+    
+    db = establish_sql_connection()
+    cursor = db.cursor()
+    print(f'db established: {db.is_connected()}')
+
+    file = open(filepath, 'r')
+    values = file.read().split("\n")
+    values = list(map(lambda x: f'(\'{x}\')',values))
+    values = ", ".join(values)
+    query = f'INSERT INTO User_roles(role) VALUES {values};'
+
+    cursor.execute(query)
+    cursor.close()
+    db.close()
+
+    return query
+
+add_user_roles('User_roles.txt')
+
+
+
+# cursor = db.cursor()
+#     query = "INSERT INTO\
+#         FROM customers\
+#             ORDER BY customer_id DESC"
+#     cursor.execute(query)
+#     result = cursor.fetchall()
+
+
