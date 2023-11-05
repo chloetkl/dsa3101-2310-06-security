877ef24e607c7deb318cd678f7604c2d730de278
chloetkl <e0889098@u.nus.edu>
2023-11-05 11:08:58 +0800

    Docker compose succesfully runs default.py, adding user_roles in the database

--- back-end/database/populate/defaults.py
+++ back-end/database/populate/defaults.py
@@ -3,31 +3,38 @@ import pandas as pd
 import bcrypt
 
 def establish_sql_connection():
-    # note: for testing purposes, run on localhost
-    db = mysql.connector.connect(
-        host = "localhost",
-        port = 1001,
-        user = "root",
-        password = "dsa3101",
-        database = "secdb"
+    db = None  # Initialize db and cursor variables to None
+    cursor = None
+    try:
+        # note: for testing purposes, run on localhost
+        # db = mysql.connector.connect(
+        #     host = "localhost",
+        #     port = 1001,
+        #     user = "root",
+        #     password = "dsa3101",
+        #     database = "secdb"
+        #     )
+
+        # uncomment below for dockerised usage
+        db = mysql.connector.connect(
+            host="database",
+            user="root",
+            password="dsa3101",
+            database="secdb"
         )
-    # uncomment below for dockerised usage
-    # db = mysql.connector.connect(
-    #     host = "database",
-    #     user = "root",
-    #     password = "dsa3101",
-    #     database = "secdb"
-    #     )
-    return db
+
+        cursor = db.cursor()
+    except mysql.connector.Error as err:
+        print(f"establish_sql_connection | Database error: {err}")
+    return db, cursor
 
 # user_roles
 def add_user_roles(filepath):
 
     try: 
         ## establish connection
-        db = establish_sql_connection()
-        cursor = db.cursor()
-        print(f'db established: {db.is_connected()}')
+        db, cursor = establish_sql_connection()
+        print(f'add_user_role | db established: {db.is_connected()}')
 
         ## read users from path and change to QUERY format
         file = open(filepath, 'r')
@@ -38,22 +45,22 @@ def add_user_roles(filepath):
 
         cursor.execute(query)
         db.commit()
-        print(f"Number of rows affected: {cursor.rowcount}")
+        print(f"add_user_role | User roles added: {cursor.rowcount}")
 
-    except mysql.connector.Error as err:
-        print(f"Database error: {err}")
-
-    finally:
         cursor.close()
         db.close()
 
-# add users
+    except mysql.connector.Error as err:
+        print(f"add_user_role | database error: {err}")
+
+        
+
+## ADD user
 def add_user(username, password, role, email=None):
     try: 
         ## establish connection
-        db = establish_sql_connection()
-        cursor = db.cursor()
-        print(f'db established: {db.is_connected()}')
+        db, cursor = establish_sql_connection()
+        print(f'add_user | db established: {db.is_connected()}')
 
         ## generate hash and salt
         salt = bcrypt.gensalt()
@@ -72,76 +79,64 @@ def add_user(username, password, role, email=None):
             VALUES (\'{email}\', \'{username}\', {role_id}, \'{salt.decode('utf-8')}\', \'{hash.decode('utf-8')}\')"
         cursor.execute(query)
         db.commit()
+        print(f"add_user | Users added: {cursor.rowcount}")
 
-    except mysql.connector.Error as err:
-        print(f"Database error: {err}")
-
-    finally:
         cursor.close()
         db.close()
 
-# authenticate user
+    except mysql.connector.Error as err:
+        print(f"add_user | database error: {err}")
+
+## AUTHENTICATE user
 def authenticate(user,password):
     try:
-        db = establish_sql_connection()
-        cursor = db.cursor()
+        db, cursor = establish_sql_connection()
         query = f"SELECT hash from Users where username = \'{user}\'"
         cursor.execute(query)
         hash_stored = cursor.fetchall()
 
         ## Check username
         if len(hash_stored) == 0:
-            print("User not found")
+            print("authenticate | User not found")
             return False
         
-        ## Check password
+        ## Pull shash salt
         hash_stored = hash_stored[0][0]
         query = f"SELECT salt from Users where username = \'{user}\'"
         cursor.execute(query)
         salt = cursor.fetchall()[0][0]
         hash_entered = bcrypt.hashpw(password.encode('utf-8'),salt.encode('utf-8'))
-    
-    except mysql.connector.Error as err:
-        print(f"Database error: {err}")
-    
-    finally:
+
         cursor.close()
         db.close()
+    
+    except mysql.connector.Error as err:
+        print(f"authenticate | database error: {err}")
+
 
     ## Check results
-    authenticated = (hash_stored == hash_entered.decode('utf-8'))
+    authenticated = bcrypt.checkpw(password.encode('utf-8'),hash_stored.encode('utf-8'))
     
     return authenticated
 
-# incident_location_groups
+
+# ADD incident_location_groups
 
 
-# incident_location
+# ADD incident_location
 
 
-# incident_types
+# ADD incident_types
 
-## perform adds
+## perform default adds
 add_user_roles('User_roles.txt')
 
-
-
 ## Checks
-db = establish_sql_connection()
-cursor = db.cursor()
-query = "SELECT * from User_roles"
-cursor.execute(query)
-User_roles = cursor.fetchall()
-print(f'User_roles: {User_roles}')
-cursor.close()
-db.close()
-
-
+# db = establish_sql_connection()
 # cursor = db.cursor()
-#     query = "INSERT INTO\
-#         FROM customers\
-#             ORDER BY customer_id DESC"
-#     cursor.execute(query)
-#     result = cursor.fetchall()
-
-
+# query = "SELECT * from User_roles"
+# cursor.execute(query)
+# User_roles = cursor.fetchall()
+# print(f'User_roles: {User_roles}')
+# cursor.close()
+# db.close()
