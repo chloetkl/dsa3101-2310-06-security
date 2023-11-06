4d85d4e6fcec1cd502ed9452bba19139d76e6727
chloetkl <e0889098@u.nus.edu>
2023-11-07 01:37:09 +0800

    Fixed html display and csv file updates connection

--- main/draft/Testing/nusecure.py
+++ main/draft/Testing/nusecure.py
@@ -17,31 +17,31 @@ def home():
             return "Invalid UserID or Password"
     return render_template('home.html')
 
-def update_csv(new):
-    df = pd.read_csv('./data/data.csv')
-    df = df.append(new, ignore_index=True)
-    df.to_csv('./data/data.csv', index=False)
+# def update_csv(new):
+#     df = pd.read_csv('./data/data.csv')
+#     df = df.append(new, ignore_index=True)
+#     df.to_csv('./data/data.csv', index=False)
 
 @app.route('/security', methods=['GET', 'POST'])
 def security():
-    if request.method == 'POST':
-        new_report = {
-            'IncidentID': request.form['id'],
-            'Description': request.form['description'],
-            'Incidents': request.form['type'],
-	    'FirstUpdate': request.form['datetime'],
-	    'Priority': request.form['priority'],
-            'Location': request.form['location'],
-	    'Building': request.form['building'],
-	    'Status': request.form['status'],
-	    'User': request.form['user'],
-	    'Latitude': request.form['latitude'],
-	    'Longitude': request.form['longitude']
-        }
+    # if request.method == 'POST':
+    #     new_report = {
+    #         'IncidentID': request.form['id'],
+    #         'Description': request.form['description'],
+    #         'Incidents': request.form['type'],
+	#     'FirstUpdate': request.form['datetime'],
+	#     'Priority': request.form['priority'],
+    #         'Location': request.form['location'],
+	#     'Building': request.form['building'],
+	#     'Status': request.form['status'],
+	#     'User': request.form['user'],
+	#     'Latitude': request.form['latitude'],
+	#     'Longitude': request.form['longitude']
+    #     }
 
-        update_csv(new_report)
+    #     update_csv(new_report)
 
-    data = pd.read_csv('./data/data.csv')
+    data = pd.read_csv('data/data_test.csv')
     data_dict = data.to_dict(orient='records')
     return render_template('security_table.html', data=data_dict)
 
@@ -50,4 +50,4 @@ def analytics():
     return render_template('analytics.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5010, debug=True)
