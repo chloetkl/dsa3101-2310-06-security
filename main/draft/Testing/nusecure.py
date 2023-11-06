6ac1e616b4a52bf9c6b04e25f50c9a61c2888ac4
chloetkl <e0889098@u.nus.edu>
2023-11-07 01:39:35 +0800

    Changed port on nusecure.py back to 5000. Note syncing to data_test.csv is just a way to check whether tables update live, reupdate codes

--- main/draft/Testing/nusecure.py
+++ main/draft/Testing/nusecure.py
@@ -50,4 +50,4 @@ def analytics():
     return render_template('analytics.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5010, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
