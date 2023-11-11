c20c53340d72cfc8455a9cc0c7e45d9fb7112c13
sumiko <tengwj01sumiko@gmail.com>
2023-11-11 15:09:43 +0800

    Added endpoints and scripts for to get forecast for each incident type

--- back/models/sarima/feature_eng.py
+++ back/models/sarima/feature_eng.py
@@ -2,17 +2,17 @@ import pandas as pd
 
 def engineer_features(data):
     data['Open'] = pd.to_datetime(data['Open'])
-    data['Open'].sort_values()
+    data.sort_values('Open', inplace=True)
+
     data.set_index('Open', inplace=True)
 
-    split_date = pd.Timestamp('2019-01-01')
-    data = data.loc[data.index >= split_date]
-    split_date = pd.Timestamp('2023-10-15')
-    data = data.loc[data.index <= split_date]
-    split_date = pd.Timestamp('2022-08-31')
-    train = data.loc[data.index <= split_date]
-    test = data.loc[data.index > split_date]
+    weekly_data = data['IncidentID'].resample('W').count()
 
-    weekly_incidents = train['IncidentID'].resample('W').count()
-    return weekly_incidents
-    
+    last_date = weekly_data.index[-1]
+
+    split_date_1_year = last_date - pd.DateOffset(years=1)
+
+    train = weekly_data.loc[weekly_data.index <= split_date_1_year]
+    test = weekly_data.loc[weekly_data.index > split_date_1_year]
+
+    return weekly_data, train, test
--- nusecure.py
+++ nusecure.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, send_file, render_template
-from back.models.sarima.fit_sarima_model import fit_sarima_model
+from flask import Flask, request, send_file, render_template, jsonify
+from back.models.sarima.sarima_model import forecast_all_sarima, train_all_sarima
 
 import mysql.connector
 import bcrypt
-from back_end.database.populate import add_user, check_auth
+# from back_end.database.populate import add_user, check_auth
 
 app = Flask(__name__)
 
@@ -16,26 +16,50 @@ def nusecure():
 def send_p():
     return send_file("back/data/test.png")
 
-@app.route("/add_user", methods = ["POST"])
-def add_user(email, username, role_id, password):
-    email = request.args.get('email')
-    username = request.args.get('username')
-    role_id = request.args.get('role_id')
-    password = request.args.get('password')
-    salt = bcrypt.gensalt()
-    hash = bcrypt.hashpw(password, salt)
+# @app.route("/add_user", methods = ["POST"])
+# def add_user(email, username, role_id, password):
+#     email = request.args.get('email')
+#     username = request.args.get('username')
+#     role_id = request.args.get('role_id')
+#     password = request.args.get('password')
+#     salt = bcrypt.gensalt()
+#     hash = bcrypt.hashpw(password, salt)
 
-    db = establish_sql_connection()
-    cursor = db.cursor
-    query = f"\
-        INSERT INTO Users\
-        (email, username, role_id, salt, hash)\
-        VALUES ('{email}', '{username}', {role_id}, '{salt}', '{hash}')"
+#     db = establish_sql_connection()
+#     cursor = db.cursor
+#     query = f"\
+#         INSERT INTO Users\
+#         (email, username, role_id, salt, hash)\
+#         VALUES ('{email}', '{username}', {role_id}, '{salt}', '{hash}')"
 
-@app.route("/fit_sarima")
-def fit_sarima():
-    fit_sarima_model()
-    return "sarima pkl added in container"
+# Endpoint to train SARIMA models for all incident types
+@app.route('/train_all', methods=['GET'])
+def train_all():
+    incident_types = ['LOST AND FOUND','DAMAGED PROPERTY','SEXUAL INCIDENTS','STOLEN ITEMS','EMERGENCY INCIDENTS']
+    train_all_sarima(incident_types)
+    return jsonify({'message': 'All models trained successfully'})
+
+# Endpoint to generate forecast for all incident types
+@app.route('/forecast_all', methods=['GET'])
+def get_all_forecasts():
+    incident_types = ['LOST AND FOUND','DAMAGED PROPERTY','SEXUAL INCIDENTS','STOLEN ITEMS','EMERGENCY INCIDENTS']
+    forecast_all_sarima(incident_types)
+    return jsonify({'message': 'Forecasts generated for all incident types'})
+
+# Endpoint to get forecast plots for specified type
+@app.route('/get_forecast_plot', methods=['GET'])
+def get_forecast_plot():
+    incident_type = request.args.get('incident_type', default=False)
+
+    if incident_type:
+        plot_file = f'back/models/sarima/forecast_plot/forecast_plot_{incident_type}.png'
+    else:
+        plot_file = f'back/models/sarima/forecast_plot/forecast_plot_False.png'
+
+    try:
+        return send_file(plot_file, mimetype='image/png')
+    except FileNotFoundError:
+        return 'File not found.', 404
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
