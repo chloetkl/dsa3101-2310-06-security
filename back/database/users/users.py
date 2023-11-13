9e7e9a73e30d87d936196332fb4f1709d33bcabb
chloetkl <e0889098@u.nus.edu>
2023-11-13 11:21:48 +0800

    Debugged log-in page. Changed users.py to connect from docker server. Also copied front into flask_dockerfile

--- back/database/users/users.py
+++ back/database/users/users.py
@@ -11,21 +11,21 @@ def establish_sql_connection(max_retries=10, retry_interval=10):
     while retries < max_retries:
         try:
             # note: for testing purposes, run on localhost
-            db = mysql.connector.connect(
-                host = "localhost",
-                port = 1001,
-                user = "root",
-                password = "dsa3101",
-                database = "secdb"
-                )
+            # db = mysql.connector.connect(
+            #     host = "localhost",
+            #     port = 1001,
+            #     user = "root",
+            #     password = "dsa3101",
+            #     database = "secdb"
+            #     )
 
             # uncomment below for dockerised usage
-            # db = mysql.connector.connect(
-            #     host="database",
-            #     user="root",
-            #     password="dsa3101",
-            #     database="secdb"
-            # )
+            db = mysql.connector.connect(
+                host="database",
+                user="root",
+                password="dsa3101",
+                database="secdb"
+            )
 
             cursor = db.cursor()
             break  # If connection succeeds, exit the loop
