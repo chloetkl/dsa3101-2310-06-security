040a97edcd9c6cdbed3b03377df89da6c4b04a8c
chloetkl <e0889098@u.nus.edu>
2023-11-15 01:57:02 +0800

    Add log out end-point

--- nusecure.py
+++ nusecure.py
@@ -275,7 +275,13 @@ def rank_priority():
     except TypeError:
         return 'Input not found.', 404
     
-
+@app.route('/logout', methods=['GET'])
+def logout():
+    try:
+        logout_user()
+        return redirect(url_for('home')), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
