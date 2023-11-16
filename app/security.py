b44dd752a976718ffe888632f7fdc8542f8a9e04
chloetkl <e0889098@u.nus.edu>
2023-11-16 16:08:58 +0800

    security page tried to reconfigure updateIncident

--- app/security.py
+++ app/security.py
@@ -29,64 +29,69 @@ def security():
     user_id = current_user.id
     print(f"User {user_id} is authenticated")
 
-    db,cursor = establish_sql_connection()
-    query = f'SELECT Incident_logs.incident_id,\
-        Incidents.description,\
-        Incident_logs.priority,\
-        Incident_types.type,\
-        Incident_location_groups.location_group,\
-        Incident_locations.location,\
-        Incident_locations.latitude,\
-        Incident_locations.longitude,\
-        Users.username,\
-        Incident_logs.time,\
-        Incident_logs.status\
-        FROM Incident_logs\
-        LEFT JOIN Incidents ON Incident_logs.incident_id = Incidents.id\
-        LEFT JOIN Incident_locations ON Incidents.location_id = Incident_locations.id\
-        LEFT JOIN Incident_location_groups ON Incident_locations.location_group_id = Incident_location_groups.id\
-        LEFT JOIN Incident_types ON Incidents.incident_type_id = Incident_types.id\
-        LEFT JOIN Users ON Incident_logs.user_id = Users.id\
-        ORDER BY Incident_logs.time DESC;'
-    cursor.execute(query)
-    result = cursor.fetchall()
-    columns = [desc[0] for desc in cursor.description]
-    data = pd.DataFrame(result, columns = columns)
+    try:
+        db,cursor = establish_sql_connection()
+        query = f'SELECT Incident_logs.incident_id,\
+            Incidents.description,\
+            Incident_logs.priority,\
+            Incident_types.type,\
+            Incident_location_groups.location_group,\
+            Incident_locations.location,\
+            Incident_locations.latitude,\
+            Incident_locations.longitude,\
+            Users.username,\
+            Incident_logs.time,\
+            Incident_logs.status\
+            FROM Incident_logs\
+            LEFT JOIN Incidents ON Incident_logs.incident_id = Incidents.id\
+            LEFT JOIN Incident_locations ON Incidents.location_id = Incident_locations.id\
+            LEFT JOIN Incident_location_groups ON Incident_locations.location_group_id = Incident_location_groups.id\
+            LEFT JOIN Incident_types ON Incidents.incident_type_id = Incident_types.id\
+            LEFT JOIN Users ON Incident_logs.user_id = Users.id\
+            ORDER BY Incident_logs.time DESC;'
+        cursor.execute(query)
+        result = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        data = pd.DataFrame(result, columns = columns)
 
-    # Grouping by specified columns and aggregating time column
-    data = data.groupby(['incident_id', 'description', 'priority', 'type', 'location_group',
-                                    'location', 'latitude', 'longitude']).agg(
-                                        FirstUpdate=('time', 'min'), LatestUpdate=('time', 'max'),
-                                        username=('username', 'first'), 
-                                        status=('status', 'first') 
-                                        ).reset_index() 
-    data = data.sort_values('LatestUpdate', ascending=False)
-    now = datetime.now()
-    data = data[data['LatestUpdate'] <= now]
-    data.rename(columns={'incident_id': 'IncidentID', 'description':'Description','priority': 'Priority', 
-                         'type':'Incidents', 'location_group':'Location', 'location':'Building', 
-                         'latitude':'Latitude', 'longitude':'Longitude', 'username':'User', 
-                         'time': 'LatestUpdate', 'status':'Status'}, inplace=True)
-    data['FirstUpdate'] = pd.to_datetime(data['FirstUpdate'])
-    data['LatestUpdate'] = pd.to_datetime(data['LatestUpdate'])
+        # Grouping by specified columns and aggregating time column
+        data = data.groupby(['incident_id', 'description', 'priority', 'type', 'location_group',
+                                        'location', 'latitude', 'longitude']).agg(
+                                            FirstUpdate=('time', 'min'), LatestUpdate=('time', 'max'),
+                                            username=('username', 'first'), 
+                                            status=('status', 'first') 
+                                            ).reset_index() 
+        data = data.sort_values('LatestUpdate', ascending=False)
+        now = datetime.now()
+        data = data[data['LatestUpdate'] <= now]
+        data.rename(columns={'incident_id': 'IncidentID', 'description':'Description','priority': 'Priority', 
+                            'type':'Incidents', 'location_group':'Location', 'location':'Building', 
+                            'latitude':'Latitude', 'longitude':'Longitude', 'username':'User', 
+                            'time': 'LatestUpdate', 'status':'Status'}, inplace=True)
+        data['FirstUpdate'] = pd.to_datetime(data['FirstUpdate'])
+        data['LatestUpdate'] = pd.to_datetime(data['LatestUpdate'])
 
 
-    data = data[['IncidentID', 'Description', 'Priority', 'Incidents', 'Location', 'Building', 'Latitude', 'Longitude', 'User', 'FirstUpdate', 'LatestUpdate', 'Status']]
+        data = data[['IncidentID', 'Description', 'Priority', 'Incidents', 'Location', 'Building', 'Latitude', 'Longitude', 'User', 'FirstUpdate', 'LatestUpdate', 'Status']]
 
+        
+        data['FirstUpdate'] = pd.to_datetime(data['FirstUpdate'])
+        data['Date'] = data['FirstUpdate'].dt.date
+        data['Time'] = data['FirstUpdate'].dt.time
+        data.rename(columns={'IncidentID': 'Incident ID',
+                'Incidents': 'Incident Type',
+            'User': 'User Added'}, inplace=True)
+        data = data[['Incident ID','Description','Date','Time',
+                'Incident Type','Location','Building','Status','Priority',
+                'User Added','Latitude','Longitude'
+                ]]
+        data_dict = data.to_dict(orient='records')
+
+        return render_template('security.html', data=data_dict), 200
     
-    data['FirstUpdate'] = pd.to_datetime(data['FirstUpdate'])
-    data['Date'] = data['FirstUpdate'].dt.date
-    data['Time'] = data['FirstUpdate'].dt.time
-    data.rename(columns={'IncidentID': 'Incident ID',
-             'Incidents': 'Incident Type',
-	     'User': 'User Added'}, inplace=True)
-    data = data[['Incident ID','Description','Date','Time',
-             'Incident Type','Location','Building','Status','Priority',
-             'User Added','Latitude','Longitude'
-             ]]
-    data_dict = data.to_dict(orient='records')
-
-    return render_template('security.html', data=data_dict)
+    except Exception as e:
+        return jsonify({'error': e}), 500
+    
 
 @security_bp.route('/security/update-incident', methods=['POST'])
 @login_required
@@ -106,16 +111,16 @@ def update_incident():
                 ('{incident_id}', '{status}', '{priority}', '{time}', '{user_id}', '{notes}')"
         cursor.execute(query)
         db.commit()
+        return jsonify({'message': f'Status of {incident_id} has changed to {status}'}), 200
+        
 
     except Exception as e:
-        print(f"Error: {e}")
         db.rollback()
-
+        return jsonify({'error': f'Failed to add data due to {e}'}), 500
+    
     finally:
         cursor.close()
         db.close()
-
-    return redirect(url_for('security.security'))
 
 @security_bp.route('/security/generate-map-pin', methods=['GET'])
 @login_required
